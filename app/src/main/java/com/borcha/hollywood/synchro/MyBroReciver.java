0093775479b8b39fa476fabe517b4c82dbea04ef
borcha <borisbokan@gmail.com>
2017-05-28 11:01:04 +0200

    Uradjen myDialogComment ....bla bla

--- app/src/main/java/com/borcha/hollywood/synchro/MyBroReciver.java
+++ app/src/main/java/com/borcha/hollywood/synchro/MyBroReciver.java
@@ -1,10 +1,16 @@
 package com.borcha.hollywood.synchro;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
 /**
  * Created by borcha on 28.05.17..
  */
 
 public class MyBroReciver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
